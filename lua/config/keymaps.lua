908e6f02f135d348becfac662e8be3b02fc71a34
Jhalep <jhalep2@gmail.com>
2024-05-29 21:53:11 -0500

    Add initial navigation keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Terminal
+vim.keymap.set("n", "<C-\\>", function()
+  LazyVim.terminal()
+end, { desc = "Terminal (cwd)" })
+vim.keymap.set("t", "<C-\\>", "<cmd>close<cr>", { desc = "Hide Terminal" })
+
+-- Buffers Navigation
+vim.keymap.set("n", "<M-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+vim.keymap.set("n", "<M-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
